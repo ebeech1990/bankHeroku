801c5ec93f3e03c4f5765daf9e63181152190e9c
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-04-16 14:43:46 -0400

    removed static

--- src/main/Java/app/Run.java
+++ src/main/Java/app/Run.java
@@ -4,14 +4,7 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class Run {
